191cd45615d2dd4919763378db90007b3d3c041e
EstelaLacerda <elo@cesar.school>
2023-11-01 14:02:24 -0300

    Testando se o c ta funcionando normal aq no pc pq antes tinha tido um bug. Ignora essa main pq dps vou apagar.

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Testando se o c ta funcionando normal aq no pc pq antes tinha tido um bug. Ignora essa main pq dps vou apagar. ");
+
+    return 0;
+}
