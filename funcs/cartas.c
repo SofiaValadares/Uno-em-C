73e6a62959c9a6be243b5beab33be48bde175ce6
EstelaLacerda <elo@cesar.school>
2023-11-02 10:56:23 -0300

    Adição dos números e cores nas funções em cartas

--- funcs/cartas.c
+++ funcs/cartas.c
@@ -18,58 +18,60 @@ char* nomeCarta(Baralho *card) {
 
     switch(card->simbulo) { // Codigo do Simbulo aqui
         case 0:
-            strcpy(strCard, "X");
+            strcpy(strCard, "0 ");
             break;
         case 1:
-            strcpy(strCard, "X");
+            strcpy(strCard, "1 ");
             break;
         case 2:
-            strcpy(strCard, "X");
+            strcpy(strCard, "2 ");
             break;
         case 3:
-            strcpy(strCard, "X");
+            strcpy(strCard, "3 ");
             break;
         case 4:
-            strcpy(strCard, "X");
+            strcpy(strCard, "4 ");
             break;
         case 5:
-            strcpy(strCard, "X");
+            strcpy(strCard, "5 ");
             break;
         case 6:
-            strcpy(strCard, "X");
+            strcpy(strCard, "6 ");
             break;
         case 7:
-            strcpy(strCard, "X");
+            strcpy(strCard, "7 ");
             break;
         case 8:
-            strcpy(strCard, "X");
+            strcpy(strCard, "8 ");
             break;
         case 9:
-            strcpy(strCard, "X");
+            strcpy(strCard, "9 ");
             break;
         case 10:
-            strcpy(strCard, "X");
+            strcpy(strCard, "Inverter ");
             break;
         case 11:
-            strcpy(strCard, "X");
+            strcpy(strCard, "Bloquear ");
             break;
         case 12:
-            strcpy(strCard, "X");
+            strcpy(strCard, "+2 ");
             break;
     }
 
     switch(card->tipo) { // As cores aqui
         case 1:
-            strcat(strCard, "XXX");
+            strcat(strCard, "Verde");
             break;
         case 2:
-            strcat(strCard, "XXX");
+            strcat(strCard, "Azul");
             break;
         case 3:
-            strcat(strCard, "XXX");
+            strcat(strCard, "Amarelo");
             break;
         case 4:
-            strcat(strCard, "XXX");
+            strcat(strCard, "Vermelho");
             break;
     }
+
+    return strCard;
 }
